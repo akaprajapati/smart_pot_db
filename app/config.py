debb103d806a35cdadcc0c0da32390b84653aa8c
akaprajapati <92862436+akaprajapati@users.noreply.github.com>
2024-08-15 20:02:27 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql://smart_pot_user:gia@localhost:5432/smart_pot_db"
+    database_url: str = "postgresql://smart_pot_user:gia@127.0.0.1:5432/smart_pot_db"
     secret_key: str = "your_secret_key"
 
     class Config:
